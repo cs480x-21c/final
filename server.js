4cc1ecf6dda558323a4ba0cc376ac82e0557821d
xchen0326 <rinemiya0203@gmail.com>
2021-03-15 01:20:14 -0400

    server and package structure done

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser")
+
+// make all the files in 'public' available
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+app.use(express.static("views"));
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+    response.sendFile(__dirname + "/index.html");
+});
+
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log("Your app is listening on port " + listener.address().port);
+});
