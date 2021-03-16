ec1026bc08484343b24384664068ddd29db83025
Jason Dykstra <jasondykstra2000@gmail.com>
2021-03-15 21:16:44 -0400

    added TLNA vs normalized tuition and fees

--- js/NormalizedTuitionVsTLNA.js
+++ js/NormalizedTuitionVsTLNA.js
@@ -8,12 +8,12 @@
     // Create a div for the SVG to be placed into
     var svgDiv = document.createElement("div");
     svgDiv.classList.add("svgdiv");
-    svgDiv.id = "tuitionDiv";
+    svgDiv.id = "tuitionVsTLNADiv";
 
     // Create a div for the text to go in
     var textDiv = document.createElement("div");
     textDiv.classList.add("textbox");
-    textDiv.id = "tuitionTextDiv";
+    textDiv.id = "tuitionVsTLNATextDiv";
 
     // Append divs to body
     document.body.appendChild(svgDiv);
@@ -25,7 +25,7 @@
         height = 500 - margin.top - margin.bottom;
 
     // Define svg
-    var svg = d3.select("#tuitionDiv")
+    var svg = d3.select("#tuitionVsTLNADiv")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -66,13 +66,17 @@
             return d.Year;
         }));
 
+        // Height scale
+        var heightScale = 1.05;
+
         y0.domain([0, d3.max(data, function (d) {
-            return parseInt(d.TF) * 1000;
+            return (parseInt(d.TLNA) * 1000)*heightScale;
         })]);
 
-        y1.domain([3000, d3.max(data, function(d) {
-            return parseInt(d.TS) + 1000;
+        y1.domain([0, d3.max(data, function(d) {
+            return ((parseFloat(d.TF)*1000)/parseFloat(d.TS))*1.5;
         })])
+
 
 
 
@@ -84,16 +88,19 @@
 
         // y scale
         yScale.domain([0, d3.max(data, function (d) {
-            return d.TF;
+            return d.TLNA * heightScale;
         })]);
 
 
         function hoverData(d){
             var s = d.Year + "<br>";
-            s += "Tuition and Fees: $" + (d.TF * 1000).toLocaleString("en") + "<br>" + "Total students: " +
-                d.TS + "<br>";
+            s += "Total Liabilities and Net Assets: $" + (d.TLNA * 1000).toLocaleString("en") + "<br>" +
+                "Normalized Tuition and Fees: " + "$" +
+                ((d.TF*1000)/d.TS).toLocaleString("en") + "<br>";
             return s;
         }
+
+
 
 
         // Draw bar chart
@@ -105,7 +112,7 @@
                 return xScale(d.Year);
             })
             .attr("y", function (d) {
-                return yScale(d.TF);
+                return yScale(d.TLNA);
             })
             .attr("width", xScale.bandwidth())
             .style("fill", barColor)
@@ -114,16 +121,16 @@
 
             })
             .attr("height", function (d) {
-                return height - yScale(d.TF);
+                return height - yScale(d.TLNA);
             })
             .on('mouseover', function (d) {
                 d3.select(this).style("fill", barDownColor);
-                var div = document.getElementById("tuitionTextDiv");
+                var div = document.getElementById("tuitionVsTLNATextDiv");
                 div.innerHTML = hoverData(d);
             })
             .on('mouseout', function (d) {
                 d3.select(this).style("fill", barColor);
-                var div = document.getElementById("tuitionTextDiv");
+                var div = document.getElementById("tuitionVsTLNATextDiv");
                 //div.innerHTML = "";
             })
 
@@ -141,7 +148,7 @@
             .attr("x", 0 - (height/2))
             .attr("dy", "1em")
             .style("text-anchor", "middle")
-            .text("Tuition And Fees")
+            .text("Total Liabilities and Net Assets")
             .style("font-size", "14px");
 
         // Add y axis right label
@@ -151,7 +158,7 @@
             .attr("x", 0 - (height/2))
             .attr("dy", "1em")
             .style("text-anchor", "middle")
-            .text("Total Students Enrolled")
+            .text("Normalized Tuition and Fees")
             .style("font-size", "14px");
 
 
@@ -162,7 +169,7 @@
             .attr("text-anchor", "middle")
             .style("font-size", "20px")
             .style("font-weight", "bold")
-            .text("Tuition And Fees, Total Students Enrolled vs Year");
+            .text("Total Liabilities and Net Assets vs Normalized Tuition and Fees");
 
 
 
@@ -174,7 +181,7 @@
             .attr("stroke-width", 3)
             .attr("d", d3.line()
                 .x(function(d) { return xScale(d.Year) + (xScale.bandwidth() / 2); })
-                .y(function(d) { return y1(d.TS); })
+                .y(function(d) { return y1((d.TF*1000)/d.TS); })
             );
 
         // Append dots to line chart
@@ -186,7 +193,7 @@
             .attr("class", "dot")
             .attr("fill", lineColor)
             .attr("cx", function(d) { return xScale(d.Year) + (xScale.bandwidth() / 2); })
-            .attr("cy", function(d) { return y1(d.TS); })
+            .attr("cy", function(d) { return y1((d.TF*1000)/d.TS); })
             .attr("r", 4);
 
 
@@ -206,13 +213,13 @@
 
         // Append y axis left
         svg.append("g")
-            .attr("id", "tuitionYAxisLeft")
+            .attr("id", "tuitionVsTLNAYAxisLeft")
             .attr("class", "yAxis")
             .call(yAxisLeft);
 
         // Append y axis right
         svg.append("g")
-            .attr("id", "tuitionYAxisRight")
+            .attr("id", "tuitionVsTLNAYAxisRight")
             .attr("class", "yAxis")
             .call(yAxisRight)
             .attr("transform", "translate(" + width + ", 0)");
