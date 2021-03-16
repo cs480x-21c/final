339abf59f6f90f5618a3295c41970db66ebface1
Clay OL <clay.oshiroleavitt@gmail.com>
2021-03-15 20:30:45 -0400

    added ISO3166 Alpha-3 to JSON for ease of indexing

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import os
 import csv
 import json
 import pandas as pd
+import pycountry_convert as pc
 basepath = os.path.abspath(os.path.dirname(__file__))
 path= basepath + "/chart_scrapes"
 
@@ -259,6 +260,7 @@ def weekly_meta_calc(weeks, countries):
                     # weekAverages.to_csv(outputPath + "_average_meta.csv")
         country_data.append(
             {"country":country,
+            "Alpha-3" : pc.country_name_to_country_alpha3(pc.country_alpha2_to_country_name(country.upper())),
             "data":data}
         )
     with open('Datasets/Master_JSON.json', 'w') as json_file:
